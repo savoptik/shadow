9a4b9605eb8529874240a9afe65e7356b9e41326
Mikhail Efremov <sem@altlinux.org>
2015-10-06 20:15:46 +0300

    Revert "Move selinux_file_context out of libmisc/copydir.c"
    This all was changed in the upstream: there is no
    selinux_file_context() anymore.
    This reverts commit 3a96cf45d24e99939ec1c6e8ddef876d799c48a4.
    
    Conflicts:
    	shadow/lib/Makefile.am
    	(because of previous revert)

--- shadow/libmisc/copydir.c
+++ shadow/libmisc/copydir.c
@@ -77,6 +77,48 @@ static int copy_file (const char *src, const char *dst,
                       const struct stat *statp, const struct timeval mt[],
                       long int uid, long int gid);
 
+#ifdef WITH_SELINUX
+/*
+ * selinux_file_context - Set the security context before any file or
+ *                        directory creation.
+ *
+ *	selinux_file_context () should be called before any creation of file,
+ *	symlink, directory, ...
+ *
+ *	Callers may have to Reset SELinux to create files with default
+ *	contexts:
+ *		setfscreatecon (NULL);
+ */
+int selinux_file_context (const char *dst_name)
+{
+	static bool selinux_checked = false;
+	static bool selinux_enabled;
+	security_context_t scontext = NULL;
+
+	if (!selinux_checked) {
+		selinux_enabled = is_selinux_enabled () > 0;
+		selinux_checked = true;
+	}
+
+	if (selinux_enabled) {
+		/* Get the default security context for this file */
+		if (matchpathcon (dst_name, 0, &scontext) < 0) {
+			if (security_getenforce () != 0) {
+				return 1;
+			}
+		}
+		/* Set the security context for the next created file */
+		if (setfscreatecon (scontext) < 0) {
+			if (security_getenforce () != 0) {
+				return 1;
+			}
+		}
+		freecon (scontext);
+	}
+	return 0;
+}
+#endif
+
 /*
  * remove_link - delete a link from the linked list
  */
